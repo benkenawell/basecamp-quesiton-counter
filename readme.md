f0027c5519f41bfab896e23c2926dc6a939db5af
Ben Kenawell <ben@benkenawell.com>
2020-09-13 22:38:14 -0700

    wrote build_client function, now receving project information and parsing down to questionnaire

--- readme.md
+++ readme.md
@@ -9,3 +9,24 @@ The goal of this project is to download the answers to those questions collect t
 BaseCamp 3 requires OAuth 2 to access their API.  This is much more difficult than a simple Access Token, but worth the extra security.  However, it seems BaseCamp does not return a valid "token_type" when using the oauth2 crate's BasicClient.  Luckily the Wunderlist example from their site has a SpecialClient I've used here to get around that.  Also worth noting is that BaseCamp expects client id and client secret to be part of the request body when exchanging the code for an access token.  This is not the default option for oauth2's BasicClient.  But with the help of all the example's the oauth2 crate gave me, I was able to get it up and running!
 
 The config for the oauth I have stored in a local json config file.  Obviously this can't go into version control, but it's nice to have it stored close by!  Additionally, it was a good starter on serializing json, which will be a lot of this program once I interact with the Basecamp API.
+
+
+## API Path
+
+Once I have OAuth working, I'm off to the races!  Basecamp is a pretty full featured project management suite, and they expose everything through their API!  What that means is now I need to figure out how to get from my authenticated identity to the project question I want to aggregate the data from. Paths are below:
+
+1. [GET https://launchpad.37signals.com/authorization.json](https://github.com/basecamp/api/blob/master/sections/authentication.md#get-authorization)
+2. [GET /projects.json](https://github.com/basecamp/bc3-api/blob/master/sections/projects.md)
+3. [GET /buckets/1/questionnaires/2.json](https://github.com/basecamp/bc3-api/blob/master/sections/questionnaires.md)
+4. [GET /buckets/1/questionnaires/2/questions.json](https://github.com/basecamp/bc3-api/blob/master/sections/questions.md)
+5. [GET /buckets/1/questions/2/answers.json](https://github.com/basecamp/bc3-api/blob/master/sections/question_answers.md)
+
+
+TODO: many of the API endpoints return paginated content.  May need to page through content to aggregate effectively
+
+
+## Upgrades
+
+- Save access token, so I don't need to authorize every run
+  - potentially saves me a few seconds every test run
+  - could I just save this to an untracked file? there must be a better way, but that could work for this simple project...
--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
     let json: Creds = serde_json::from_str(&contents).expect("JSON was not well-formatted");
-    println!("json {:?}", json);
+    // println!("json {:?}", json);
 
     let at = oauth::get_auth_token(json.client_id, json.client_secret)?;
-    println!("main at {:?}", at);
-    println!("{:#?}", get_data()?);
+    // println!("main at {:?}", at);
 
-    let user_info = get_auth_endpoint(at.access_token())?;
+    let client = build_client(at.access_token())?;
+
+    // 1. get the user information
+    let user_info = client.get("https://launchpad.37signals.com/authorization.json")
+        .send()?
+        .json::<AuthEndpoint>()?;
     println!("auth response {:#?}", user_info);
+
+    // 2. get the projects of the user
+    let base_url = &user_info.accounts[0].href;
+    let project_url = [base_url.to_string(), "/projects.json".to_string()].concat();
+    let projects = client.get(&project_url).send()?.text()?;
+    // println!("projects {:#?}", projects); // prints excessive content
+    let projects_json: serde_json::Value = serde_json::from_str(&projects)?; // could create the concrete type to parse to instead here
+    println!("Project: {} \n Purpose: {}",projects_json[0]["name"], projects_json[0]["purpose"]);
+
+    // print out the names of all the projects
+    let project_names: Vec<&str> = projects_json.as_array().unwrap().iter()
+        .map(|project| project["name"].as_str().unwrap()).collect(); // must use as_str, NOT to_string
+    println!("All project names: {:?}", project_names );
+    
+    // get the project I want
+    let family_project = projects_json.as_array().unwrap().iter()
+        .find(|project| project["name"].as_str().unwrap().eq(&String::from("Family"))).unwrap();
+    println!("Family Project: {}", family_project);
+    
+    // use the dock property to find the "questionnaire" value of the "name" property
+    let questionnaire = family_project["dock"].as_array().unwrap().iter()
+        .find(|dock| dock["name"].as_str().unwrap().eq(&String::from("questionnaire"))).unwrap();
+    println!("Questionnaire: {}", questionnaire);
+
+    // 3. may not need to get the questionnaires... there might be one per project?
+
+    // 4. get the questions
+    let base_url = questionnaire["app_url"].as_str().unwrap();
+    let questions_url = [base_url.to_string(), "/questions.json".to_string()].concat();
+    let questions = client.get(&questions_url).send()?.text()?; // TODO, why does this return nothing?
+    println!("questions! {:?}", questions);
 
     Ok(())
 }
@@ -56,6 +91,14 @@ struct Account {
     name: String,
     href: String,
     app_href: String,
+}
+
+fn build_client (at: &oauth2::AccessToken) -> Result<reqwest::blocking::Client, reqwest::Error> {
+    let mut bearer_token = "Bearer ".to_string();
+    bearer_token.push_str(&at.secret().to_string());
+    let mut auth_header = reqwest::header::HeaderMap::new();
+    auth_header.insert(AUTHORIZATION, HeaderValue::from_str(&bearer_token).unwrap());
+    reqwest::blocking::ClientBuilder::new().default_headers(auth_header).user_agent("Run Counter").build()
 }
 
 fn get_auth_endpoint(at: &oauth2::AccessToken) -> Result<AuthEndpoint, reqwest::Error>{
